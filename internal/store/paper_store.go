ea59b490d2b15cbf2e21d3d976ef72e3ef8985a5
aserawayneasera <27858318+aserawayneasera@users.noreply.github.com>
2026-02-25 18:26:10 +0900

    Add PaperStore interface and ListParams struct

---
+++ internal/store/paper_store.go
@@ -0,0 +1,19 @@
+package store
+
+import "github.com/yourname/research-tracker/internal/models"
+
+type ListParams struct {
+	Status string
+	Year   int
+	Q      string
+	Limit  int
+	Offset int
+}
+
+type PaperStore interface {
+	Create(p *models.Paper) (*models.Paper, error)
+	Get(id int64) (*models.Paper, error)
+	Update(id int64, p *models.Paper) (*models.Paper, error)
+	Delete(id int64) error
+	List(params ListParams) ([]models.Paper, int, error)
+}
